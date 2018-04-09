c04931124f256f7a09ab12748d354aee9de99853
Serena Gupta <serena.gupta@grandrounds.com>
2018-04-09 03:48:26 -0700

    [(master)] Load in lib automically

--- gist/config/application.rb
+++ gist/config/application.rb
@@ -22,5 +22,8 @@ module Gist
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # http://guides.rubyonrails.org/autoloading_and_reloading_constants.html
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
